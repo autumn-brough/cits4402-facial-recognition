1991122c7e4c56312e3c79e2dc12321e1bab18bd
autumn-brough <broughautumn@gmail.com>
2021-04-23 09:57:53 +0800

    added mike's new code

--- Face_Detection.m
+++ Face_Detection.m
@@ -24,7 +24,7 @@ function varargout = Face_Detection(varargin)
 
 % Edit the above text to modify the response to help Face_Detection
 
-% Last Modified by GUIDE v2.5 16-Apr-2021 09:32:53
+% Last Modified by GUIDE v2.5 21-Apr-2021 11:35:58
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -65,9 +65,9 @@ guidata(hObject, handles);
 
 
 % --- Outputs from this function are returned to the command line.
-function delete_files_in_training_and_test_sets()
-TestFaceDataset = 'E:\UWA\Computer Vision\Project\TestFaceDataset';
-TrainingFaceDataset= 'E:\UWA\Computer Vision\Project\TrainingFaceDataset';
+function delete_files_in_training_and_test_sets(TestFaceDataset,TrainingFaceDataset)
+%TestFaceDataset = 'E:\UWA\Computer Vision\Project\TestFaceDataset';
+%TrainingFaceDataset= 'E:\UWA\Computer Vision\Project\TrainingFaceDataset';
 
 SubDirInTestFaceDataset= dir(TestFaceDataset);
 idxSubDir = [SubDirInTestFaceDataset.isdir];
@@ -86,24 +86,27 @@ for i= 1:numel(TestFaceDatasetfolders)
 end  
 
 
-function middle_split()
-FaceDataset = 'E:\UWA\Computer Vision\Project\FaceDataset';
-TestFaceDataset = 'E:\UWA\Computer Vision\Project\TestFaceDataset';
-TrainingFaceDataset= 'E:\UWA\Computer Vision\Project\TrainingFaceDataset';
+function middle_split(FaceDataset, TestFaceDataset,TrainingFaceDataset)
+%FaceDataset = 'E:\UWA\Computer Vision\Project\FaceDataset';
+%TestFaceDataset = 'E:\UWA\Computer Vision\Project\TestFaceDataset';
+%TrainingFaceDataset= 'E:\UWA\Computer Vision\Project\TrainingFaceDataset';
 
 SubDirInFaceDataset= dir(FaceDataset);
 idxSubDir = [SubDirInFaceDataset.isdir];
+% folders in FaceDataset 
 FaceDatasetfolders = {SubDirInFaceDataset(idxSubDir).name};
 FaceDatasetsubdir = cell(1,numel(FaceDatasetfolders));
 
 
 SubDirInTestFaceDataset= dir(TestFaceDataset);
 idxSubDir = [SubDirInTestFaceDataset.isdir];
+%Folders in TestFaceDataset
 TestFaceDatasetfolders = {SubDirInTestFaceDataset(idxSubDir).name};
 TestFaceDatasetsubdir = cell(1,numel(TestFaceDatasetfolders));
 
 SubDirInTrainFaceDataset= dir(TrainingFaceDataset);
 idxSubDir = [SubDirInTrainFaceDataset.isdir];
+%Folders in TrainFaceDataset
 TrainFaceDatasetfolders = {SubDirInTrainFaceDataset(idxSubDir).name};
 TrainFaceDatasetsubdir = cell(1,numel(TrainFaceDatasetfolders));
 
@@ -130,10 +133,10 @@ for i= 1:numel(FaceDatasetfolders)
   end
 end 
 
-function interleave()
-FaceDataset = 'E:\UWA\Computer Vision\Project\FaceDataset';
-TestFaceDataset = 'E:\UWA\Computer Vision\Project\TestFaceDataset';
-TrainingFaceDataset= 'E:\UWA\Computer Vision\Project\TrainingFaceDataset';
+function interleave(FaceDataset, TestFaceDataset,TrainingFaceDataset)
+%FaceDataset = 'E:\UWA\Computer Vision\Project\FaceDataset';
+%TestFaceDataset = 'E:\UWA\Computer Vision\Project\TestFaceDataset';
+%TrainingFaceDataset= 'E:\UWA\Computer Vision\Project\TrainingFaceDataset';
 
 SubDirInFaceDataset= dir(FaceDataset);
 idxSubDir = [SubDirInFaceDataset.isdir];
@@ -194,14 +197,20 @@ function Select_Image_Partitions_Callback(hObject, eventdata, handles)
 str = get(hObject, 'String');
 val = get(hObject,'Value');
 % Set current data to the selected data set.
+FaceDataset = 'E:\UWA\Computer Vision\Project\FaceDataset';
+TestFaceDataset = 'E:\UWA\Computer Vision\Project\TestFaceDataset';
+TrainingFaceDataset= 'E:\UWA\Computer Vision\Project\TrainingFaceDataset';
+handles = guidata(hObject);
+handles.TestFaceDataSet = TestFaceDataset;
+handles.TrainingFaceDataset= TrainingFaceDataset;
 switch str{val}
 case 'Middle Split' % User selects middle split
    disp('middle split')
-   delete_files_in_training_and_test_sets()
-   middle_split()
+   delete_files_in_training_and_test_sets(TestFaceDataset,TrainingFaceDataset)
+   middle_split(FaceDataset, TestFaceDataset,TrainingFaceDataset)
 case 'Interleave' % User selects interleave
-   delete_files_in_training_and_test_sets()
-   interleave()
+   delete_files_in_training_and_test_sets(TestFaceDataset,TrainingFaceDataset)
+   interleave(FaceDataset, TestFaceDataset,TrainingFaceDataset)
 end
 % Save the handles structure.
 guidata(hObject,handles)
@@ -269,3 +278,78 @@ else
    guidata(hObject, handles); %Update the handle so that handles.image_opened is accessable
                               %by the other UI interface components
 end
+
+
+% --- Executes on button press in Train_On_Image_Gallery.
+function Train_On_Image_Gallery_Callback(hObject, eventdata, handles)
+% hObject    handle to Train_On_Image_Gallery (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+handles = guidata(hObject);
+TrainFaceDataset = handles.TrainingFaceDataset;
+SubDirInTrainFaceDataset= dir(TrainFaceDataset);
+idxSubDir = [SubDirInTrainFaceDataset.isdir];
+%Folders for each face in TrainFaceDataset
+TrainFaceDatasetfolders = {SubDirInTrainFaceDataset(idxSubDir).name};
+TrainFaceDatasetsubdir = cell(1,numel(TrainFaceDatasetfolders));
+X= zeros(10304, 5,40);
+handles.X = X;
+handles.NumTrainFaceDatsetfolders = numel(TrainFaceDatasetfolders); 
+%Loop through Training face subdirectories
+for i= 1:numel(TrainFaceDatasetfolders) 
+ %TrainFaceDatasetsubdir{i}
+ P = fullfile(TrainFaceDataset,TrainFaceDatasetfolders{i});
+ %Image files in TrainFaceDataset/TrainFaceDatasetfolders
+ D = dir(fullfile(TrainFaceDataset,TrainFaceDatasetfolders{i},'*.pgm'));   %dir
+ C= cell(size(D));
+ %SizeImageFiles = cell(size(ImageFiles));
+ %TrainFaceDatasetsubdir{i} = fullfile(TrainingFaceDataset,TrainFaceDatasetfolders{i});
+ %disp(TrainFaceDatasetsubdir{i})
+ disp('folder')
+ disp(TrainFaceDatasetfolders{i})
+ %disp('number of elements in D');
+ %disp(numel(D));
+ dummyMatrix = [];
+ %Loop through images in subdirectory
+ for j = 1:numel(D)
+    C{j} = imread(fullfile(P,D(j).name));
+    imgVector = C{j}(:);
+    %Normalize vector
+    imgVector = double(imgVector);
+    imgVector= imgVector/norm(imgVector);
+    %Get its transpose
+    %imgVector = transpose(imgVector); 
+    %disp('imgVector')
+    %disp(imgVector)
+    dummyMatrix= [dummyMatrix imgVector];
+    %disp('size of dummy matrix')
+    %disp(size(dummyMatrix)); 
+    %path_name = ImageFiles(j).name; 
+    %Img = imread(fullfile(TrainFaceDatasetsubdir{i}path_name) 
+    %image = ImageFiles(j).name;  
+    disp(j)
+ end
+    %disp('size of dummy matrix')
+    %disp(size(dummyMatrix)); 
+    %disp('size of X matrix')
+    if i<= numel(TrainFaceDatasetfolders)-2
+     %disp('numel(TrainFaceDatasetfolders)')
+     %disp(numel(TrainFaceDatasetfolders));
+     %disp(size(X(:,:,i)))
+      if numel(D) ~= 0
+         X(:,:,i) = dummyMatrix;
+         handles.X(:,:,i) = dummyMatrix;
+      end
+    end  
+end
+guidata(hObject, handles);
+
+
+% --- Executes on button press in Test_for_face_with_nearest_match.
+function Test_for_face_with_nearest_match_Callback(hObject, eventdata, handles)
+% hObject    handle to Test_for_face_with_nearest_match (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+num_individuals= handles.NumTrainFaceDatsetfolders;
+for i= 1:num_individuals
+end    
